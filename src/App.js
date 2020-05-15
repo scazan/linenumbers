0c53d31be7ed6e7a5d3e87b970463c799b475604
scazan <scott@teenage.engineering>
2020-05-15 12:30:30 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
 
+const getBeats = (line) => {
+  if (line.length === 0) {
+    return '<p></p>';
+  }
+
+  const beats = line.split(/\s|-/);
+
+  const beatsHtml = beats.map((beat,i) => `<span class="beatNum">${i > 0 ? i : ''}</span><span class="beat">${beat}</span>`);
+
+  return beatsHtml.join('') + '<br/>';
+};
+
 function App() {
+  const textArea = useRef(null);
+  const [content, setContent] = useState("");
+
+  const process = () => {
+    const value = textArea.current.value;
+
+    let lines = value.split('\n');
+
+    let k = 1;
+    lines.map((line,i) => {
+      if (line.length > 0) {
+        lines[i] = `${k} ${line}`;
+        k++;
+      }
+      else {
+        k = 1;
+      }
+    });
+
+    lines = lines.map(getBeats);
+
+    setContent(lines.join('\n'));
+  };
+
+  useEffect(() => {
+    textArea.current.addEventListener('keyup', process);
+
+    return () => {
+      textArea.current.removeEventListener('keyup', process);
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        ref={textArea}
+        placeholder="add lines here. on paragraph breaks, count resets. if you type a dash, it will also count it as a syllable to right"
+      >
+      </textarea>
+
+      <div
+        className="content"
+        dangerouslySetInnerHTML={{__html: content}}
+      >
+      </div>
     </div>
   );
 }
